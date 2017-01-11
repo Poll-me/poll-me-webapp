7eb0f167c676efee39832014eb6f43eebc669d80
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2017-01-11 15:46:49 +0100

    Create user service and class

---
+++ src/app/core/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, BehaviorSubject } from 'rxjs';
+import * as _ from 'lodash';
+
+import { User } from './user';
+
+@Injectable()
+export class UserService {
+
+  public currentUser: BehaviorSubject<User>;
+  public redirectUrl: string;
+
+  constructor() {
+    this.currentUser = new BehaviorSubject<User>(undefined);
+  }
+
+  public getUser(): User {
+    return this.currentUser.getValue();
+  }
+
+  public updateUser(newUser: User): User {
+    this.currentUser.next(newUser);
+    return this.getUser();
+  }
+
+  public isValid(): boolean {
+    const user = this.getUser();
+    return !_.isUndefined(user) && !_.some(_.values(user), (val) => !val);
+  }
+}
