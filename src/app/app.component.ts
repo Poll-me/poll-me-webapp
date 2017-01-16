c6867a14bef3d633317e0417e70cc60d7035be52
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2017-01-16 09:24:03 +0100

    Add the logo to the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'pm-app',
   encapsulation: ViewEncapsulation.None,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
