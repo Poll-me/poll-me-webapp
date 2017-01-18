0ae09216cf07636147c2671db00b5fbd5724bc51
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2017-01-18 20:23:52 +0100

    Use the FirebaseUIService at login screen

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,20 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import * as _ from 'lodash';
 
+import { FirebaseUIService } from './firebaseui.service';
 import { UserService, User } from '../../core/user';
 
 @Component({
   selector: 'pm-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  providers: [ FirebaseUIService ]
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   public user: User;
 
+  private readonly fbUIContainerId = 'firebaseui-container';
+
   constructor (
+    private fbUIService: FirebaseUIService,
     private userService: UserService
   ) {
     userService.currentUser.subscribe((u) => this.user = u);
+  }
+
+  public ngOnInit() {
+    this.fbUIService.initFirebaseUI(`#${this.fbUIContainerId}`);
   }
 }
