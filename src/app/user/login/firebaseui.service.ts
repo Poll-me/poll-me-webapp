f6bdfb73eb91bc3bff8071a655b84c7d19fe09f1
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2017-01-20 11:25:47 +0100

    Add Github, Facebook and Twitter oAuth providers

--- src/app/user/login/firebaseui.service.ts
+++ src/app/user/login/firebaseui.service.ts
@@ -14,6 +14,9 @@ export class FirebaseUIService {
     signInSuccessUrl: '/',
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
     ],
     callbacks: {
