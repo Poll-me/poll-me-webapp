1cf20b02997985a092aa4c18868031e42fe3e60a
Francisco Javier Arribas Fernández <arribasjavier@gmail.com>
2017-01-25 19:20:40 +0100

    Add home module, routing and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{
+  path: '',
+  pathMatch: 'full',
+  loadChildren: './home#HomeModule',
+}, {
   path: 'user',
   loadChildren: './user#UserModule',
 }, {
   path: '**',
-  redirectTo: 'user'
+  redirectTo: ''
 }];
 
 @NgModule({
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent }   from './home.component';
+
+@NgModule({
+  declarations: [ HomeComponent ],
+  imports: [
+    SharedModule,
+    HomeRoutingModule
+  ],
+})
+export class HomeModule { }
